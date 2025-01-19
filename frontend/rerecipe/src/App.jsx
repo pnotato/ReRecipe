9f7502fef01270f5d31282332008199bad2b2c34
LeoChengZhu <leozhucheng@gmail.com>
2025-01-19 01:05:27 -0800

    added style and handle functions

--- frontend/rerecipe/src/App.jsx
+++ frontend/rerecipe/src/App.jsx
@@ -13,7 +13,7 @@ const InputBox = ({input, handleInputChange, handleSubmit}) => {
     <div>
       <input
         type = "text"
-        placeholder = "text-box"
+        placeholder = "Enter a recipe or ingredients list..."
         value = {input}
         onChange = {handleInputChange}
         className="search-bar"
@@ -37,7 +37,23 @@ const SearchButton = ({onClick}) => {
 
 function App() {
   const [input, setInput] = useState("");
-  const [springs, setSpring] = useSpring(() => ({from:{width:0}}))
+  const [calories, setCalorie] = useSpring(() => ({from:{width:0}}))
+  const [sugar, setSugar] = useSpring(() => ({from:{width:0}}))
+  const [fat, setFat] = useSpring(() => ({from:{width:0}}))
+  const [salt, setSalt] = useSpring(() => ({from:{width:0}}))
+  const [fibre, setFibre] = useSpring(() => ({from:{width:0}}))
+  const [protein, setProtein] = useSpring(() => ({from:{width:0}}))
+  const [greens, setGreens] = useSpring(() => ({from:{width:0}}))
+  const [caloriesCol, setCalorieCol] = useState('#ff6d6d')
+  const [sugarCol, setSugarCol] = useState('#ff6d6d')
+  const [fatCol, setFatCol] = useState('#ff6d6d')
+  const [saltCol, setSaltCol] = useState('#ff6d6d')
+  const [fibreCol, setFibreCol] = useState('#ff6d6d')
+  const [proteinCol, setProteinCol] = useState('#ff6d6d')
+  const [greensCol, setGreensCol] = useState('#ff6d6d')
+
+  const [ref, { width }] = useMeasure()
+  const [loadingVisible, setLoadingVisible] = useState(false);
   const [percent, setPercent] = useSpring(() => ({from:{x:0}}))
   const [transparent, setOpacity] = useState({background:'transparent'});
   const [description, setDescription] = useState("");
@@ -48,19 +64,54 @@ function App() {
     setInput(value);
   }
 
-  const handleColor = (v) => {
-    
+  function handleBars (v1, v2, v3, v4, v5, v6, v7) {
+    setCalorie.start({
+      from: {width: 0},
+      to: {width: v1},
+    })
+    setCalorieCol('#ff6d6d');
+    setSugar.start({
+      from: {width: 0},
+      to: {width: v2},
+    })
+    setSugarCol('#ff6d6d');
+    setFat.start({
+      from: {width: 0},
+      to: {width: v3},
+    })
+    setFatCol('#ff6d6d');
+    setSalt.start({
+      from: {width: 0},
+      to: {width: v4},
+    })
+    setSaltCol('#ff6d6d');
+    setFibre.start({
+      from: {width: 0},
+      to: {width: v5},
+    })
+    setFibreCol('#ff6d6d');
+    setProtein.start({
+      from: {width: 0},
+      to: {width: v6},
+    })
+    setProteinCol('#ff6d6d');
+    setGreens.start({
+      from: {width: 0},
+      to: {width: v7},
+    })
+    setGreensCol('#ff6d6d');
   }
 
-  const handleClick = () => {
-    setSpring.start({
-      from: {width: 0},
-      to: {width: 1000},
-    })
+  function handleScore(v1) {
     setPercent.start({
       from: {x:0},
-      to: {x:100},
+      to: {x:v1},
     })
+  }
+  const handleClick = () => {
+    handleBars(50, 100, 150, 200, 250, 300, 350);
+    handleScore(100);
+    setLoadingVisible(true);
     setOpacity({background:'#ffffff'});
     setDescription("Description");
     setBarPercent(100);
@@ -85,38 +136,84 @@ function App() {
 
   return (
     <div>
-      <div className="wrapper"></div>
-      <div className="container" id="left">
-        <InputBox
-            input = {input}
-            handleInputChange={handleInputChange}
-            handleSubmit={handleSubmit}
-        />
+      <InputBox
+          input = {input}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleClick}/>
+      
+      <div>
+        <div className = "bar"> 
+          <animated.div //calories
+          className = "bar-anim"
+            style={{
+            background: caloriesCol,
+            ...calories}}>
+          </animated.div>
+        </div>
+        <div className = "bar">
+          <animated.div //sugar
+            className = "bar-anim"
+            style={{
+            background: sugarCol,
+            ...sugar}}>
+          </animated.div>
+        </div>
+        <div className = "bar">
+          <animated.div //fat
+            className = "bar-anim"
+            style={{ 
+            background: fatCol,
+            ...fat}}>
+          </animated.div>
+        </div>
+        <div className = "bar">
+          <animated.div //Salt
+            className = "bar-anim"
+            style={{
+            background: saltCol,
+            ...salt}}>
+          </animated.div>
+        </div>
+        <div className = "bar">
+          <animated.div //Fibre
+            className = "bar-anim"
+            style={{
+            background: fibreCol,
+            ...fibre}}>
+          </animated.div>
+        </div>
+        <div className = "bar">
+          <animated.div //Protein
+            className = "bar-anim"
+            style={{
+            background: proteinCol,
+            ...protein}}>
+          </animated.div>
+        </div>
+        <div className = "bar">
+          <animated.div //Protein
+            className = "bar-anim"
+            style={{
+            background: greensCol,
+            ...greens}}>
+          </animated.div>
+        </div>
       </div>
-      <div className="container" id="center"></div>
-      <div className="container" id="right"></div>
-      <animated.div
-      style={{
-        height: 80,
-        background: '#ff6d6d',
-        borderRadius: 8,
-        ...springs,
-      }}>
-      </animated.div>
-      <animated.div>
-        {percent.x.to(x => x.toFixed(0))}
-      </animated.div>
+      
       <div
         style={{
           height:80,
           width:80,
           ...transparent,
         }}></div>
-      <img className = 'loading'
-      src={loading} />
+
       <h1>{description}</h1>
-      <animated.div style={{ width: 200, height: 200}}>
-        <CircularProgressbar 
+      <div className = 'score-container'>
+        <div ref = {ref} className='main'>
+          <animated.div className = 'score'>
+            {percent.x.to(x => x.toFixed(0))}
+          </animated.div>
+          <CircularProgressbar className='.score-bar'
           circleRatio={0.7}
           value= {barPercent}
           styles={{
@@ -132,7 +229,14 @@ function App() {
               stroke: '#CDFFCA'
             }
           }}></CircularProgressbar>
-      </animated.div>
+        </div>
+      </div>
+
+
+      <div>
+        {loadingVisible && <img className = 'loading'
+          src={loading} />}
+      </div>
     </div>
   )
 }
