0182c632239fe73f516fd2307191a1921a2a752e
LeoChengZhu <leozhucheng@gmail.com>
2025-01-18 17:18:45 -0800

    Added moving progress bar animation prototype

--- frontend/rerecipe/src/App.jsx
+++ frontend/rerecipe/src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import useMeasure from 'react-use-measure'
+import { useSpring, animated } from '@react-spring/web'
 import './App.css'
 
+
+const InputBox = ({input, handleInputChange, handleSubmit}) => {
+  return (
+    <div>
+      <input
+        type = "text"
+        placeholder = "text-box"
+        value = {input}
+        onChange = {handleInputChange}
+        className="search-bar"
+      />
+      <button onClick={handleSubmit} className="submit-button">
+        Submit
+      </button>
+    </div>
+  )
+} //Input box
+
+const SearchButton = ({onClick}) => {
+  return (
+    <button onClick={onClick}>
+    </button>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [springs, setSpring] = useSpring(() => ({from:{width:5}}))
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInput(value);
+  }
+  const handleClick = () => {
+    setSpring.start({
+      from: {width: 5},
+      to: {width: 100},
+      delay: 100,
+    })
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/enter-recipe-text', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: input }),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch(error) {
+      console.error('Error: ', error);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <InputBox
+          input = {input}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+      />
+      <animated.div
+      onClick={handleClick}
+      style={{
+        height: 80,
+        background: '#ff6d6d',
+        borderRadius: 8,
+        ...springs,
+      }}
+    />
+    </div>
   )
 }
 
